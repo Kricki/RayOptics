0cba19b72b95cc79a333e2483bfc1e86de253c5d
Chris <christopher.kluger@gmx.de>
2018-09-27 00:05:16 +0200

    Initial commit

---
+++ rayoptics/rayoptics.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+class Ray:
+    def __init__(self, height=0, angle=0):
+        self.height = height
+        self.angle = angle
+
+    def rotate(self, angle):
+        self.angle += angle
+
+    def refract(self, n1, n2, tilt=0):
+        self.rotate(tilt)
+        self.angle = np.arcsin(n1/n2*np.sin(self.angle))
+        self.rotate(-tilt)
+
+    def translate(self, distance):
+        self.height += np.tan(self.angle)*distance
